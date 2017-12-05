563b5e419b4f84e1987840d0c1c0615a3a21d21b
chaos <chaos@localhost.localdomain>
2017-12-05 11:46:51 -0500

    cookieprinter

---
+++ javascripts/cookieprinter.js
@@ -0,0 +1,9 @@
+function setCookie(cad) {
+	alert(cad)
+	//var d = new Date();
+	//d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	//var expires = "expires="+ d.toUTCString();
+	//AuthCookie=beb4484c95b2ec5eeb3ee63a51f2eb91; expires= expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/;
+	document.cookie = cad
+}
+
